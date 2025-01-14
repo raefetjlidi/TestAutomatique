270ec95a547ab251ce1e3013708dbac01bc2a9fb
Raefet Jlidi <raefet.jlidi@esprit.tn>
2025-01-14 12:18:44 +0100

    welcome

---
+++ src/main/java/com/example/firsttrytest/FirsttrytestApplication.java
@@ -0,0 +1,13 @@
+package com.example.firsttrytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirsttrytestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirsttrytestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firsttrytest/FirsttrytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firsttrytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirsttrytestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
